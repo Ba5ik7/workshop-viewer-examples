601089224cfd79c0251c347635fc4aa7e52d1f8d
Wesley Dusell <dusellfam@gmail.com>
2022-06-09 15:41:23 -0400

    Testing diffrent sturtures to lazy load modules.

---
+++ projects/workshop-examples/src/lib/feature-a/feature-a.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FeatureAComponent } from './feature-a.component';
+
+describe('FeatureAComponent', () => {
+  let component: FeatureAComponent;
+  let fixture: ComponentFixture<FeatureAComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FeatureAComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FeatureAComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ projects/workshop-examples/src/lib/feature-a/feature-a.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FeatureAComponent } from './feature-a.component';
+
+
+
+@NgModule({
+  declarations: [
+    FeatureAComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FeatureAModule { }
